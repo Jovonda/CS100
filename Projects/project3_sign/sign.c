fc889e0f6d75c0e929ba19f1fe3eef0642969480
Jovonda Robinson <jjrobinson2@crimson.ua.edu>
2023-03-15 20:22:41 -0500

    redid the documentation for the labs

--- Projects/project3_sign/sign.c
+++ Projects/project3_sign/sign.c
@@ -9,7 +9,11 @@ The rows are vertical and the columns are horizontal.
 This is why there is a need for input redirection.
 */
 void C_format(char s);
-int main(void) 
+void W_format(char s);
+void L_format(char s);
+void decimal_to_binary(int *array, int number);
+int gethex(char l, int num);
+int main(void)
 {
     /*
     These variables will hold the first letter
@@ -17,7 +21,7 @@ int main(void)
     printed in each line of the sign
     either being 'C', 'W', or 'L'.
 
-    The second letter specifies what character to 
+    The second letter specifies what character to
     print in the dot matrix.
     If the second character is a '~', it will print
     the charater as the first letter.
@@ -26,20 +30,153 @@ int main(void)
     */
     char first_l;
     char second_l;
-        printf("Enter formatting code:\n");
-        scanf( "%c%c",&first_l,&second_l);
-        printf("This is the first letter: %c\n",first_l);
-        printf("This is the second letter: %c\n",second_l);
-
-        if(first_l == 'C')
-        {
-            C_format(second_l);
-        }
+    printf("Enter formatting code:\n");
+    scanf("%c%c", &first_l, &second_l);
+    printf("This is the first letter: %c\n", first_l);
+    printf("This is the second letter: %c\n", second_l);
+    printf("Enter message\n");
+    if (first_l == 'C')
+    {
+        C_format(second_l);
+    }
+    else if (first_l == 'W')
+    {
+        W_format(second_l);
+    }
+    else
+    {
+        L_format(second_l);
+    }
     return 0;
 }
 
+/*
+    The program will print one space between
+    two words in output.
+    Since I can print only one character in
+    each output line, I will print the space in its
+    own line or simply print out a blank line.
+*/
 void C_format(char s)
 {
-    printf("Enter message\n");
+    printf("Here in C_format function\n");
+    char str[40];
+    char Carray[8][8] = {0};
+    int array[7] = {0};
+    int hex;
+    int count = 0;
+    while (!feof(stdin))
+    {
+        //This prevents buffer overflow, because scanf is unsafe
+        scanf("%39s", str);
+        //printf("You entered: %s\n", str);
+        for (int i = 0; i < strlen(str); i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                hex = gethex(str[i], j);
+                decimal_to_binary(array, hex);
+                for (int i = count; i < count + 1; i++)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        Carray[i][j] = array[j];
+                    }
+                }
+                count++;
+                if (count == 5)
+                {
+                    for (int i = 0; i < 8; i++)
+                    {
+                        for (int j = 0; j < 8; j++)
+                        {
+                            if (Carray[j][i] != 0)
+                            {
+                                printf("%c", s);
+                            }
+                            else
+                            {
+                                printf(" ");
+                            }
+                        }
+                        printf("\n");
+                    }
 
+                    for (int i = 0; i < 8; i++)
+                    {
+                        for (int j = 0; j < 8; j++)
+                        {
+                            Carray[j][i] = 0;
+                        }
+                    }
+                    count = 0;
+                }
+
+                for (int i = 6; i >= 0; i--)
+                {
+                    array[i] = 0;
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+/*
+    Each line of the sign contains only one word.
+*/
+void W_format(char s)
+{
+    printf("Here in W_format function\n");
+    char str[40];
+    while (!feof(stdin))
+    {
+        scanf("%s", str);
+        printf("You entered: %s\n", str);
+    }
+}
+
+/*
+    Each line of the message will be printed directly
+    as an ouput line in the sign.
+*/
+void L_format(char s)
+{
+    printf("Here in L_format function\n");
+    char line[40];
+    while (fgets(line, 100, stdin))
+    {
+        // Remove newline character from end of string
+        line[strcspn(line, "\n")] = '\0';
+
+        // Do something with the string
+        printf("Input string: %s\n", line);
+    }
+}
+
+/*
+    This functions just takes a decimal and returns a binary number
+*/
+void decimal_to_binary(int *array, int number)
+{
+    for (int i = 0; i < 7; i++)
+    {
+        array[i] = number % 2;
+        number = number / 2;
+        if (number == 0)
+        {
+            break;
+        }
+    }
+}
+
+/*
+    This function gets the 5 rows that make up that
+    letter.
+*/
+int gethex(char l, int num)
+{
+    int number = 0;
+    number = Font5x7[5 * (l - 32) + num];
+    return number;
 }
